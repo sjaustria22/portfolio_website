791236810add542119f09561ec686b05c586ee5d
Shem Justice Ruzel Austria <110037769+sjaustria22@users.noreply.github.com>
2023-01-21 19:29:35 +0800

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,10 +8,10 @@ const Hero = (props) => (
  <Section row nonpadding>
     <LeftSection>
       <SectionTitle main center>
-        Hey, I'm Shem ;)
+        Hello ;)
       </SectionTitle>
       <SectionText>
-      I design and code beautifully simple things, and I love what I do.
+      This is a sample portfolio website. I code simple things, and I love what I do.
       </SectionText>
     </LeftSection>
   </Section>
